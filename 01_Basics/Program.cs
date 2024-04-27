a5fecdc5b70c8bf7f1515720ef7e3f3d7ba097f4
Emre Kadir Tiren <77925614+emrekadirtiren@users.noreply.github.com>
2024-04-27 11:47:17 +0200

    Console Application Standard Files Upload

---
+++ 01_Basics/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Title = "The Basics";
+Console.BackgroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Hello, World!");
+//
+Console.ReadKey();
