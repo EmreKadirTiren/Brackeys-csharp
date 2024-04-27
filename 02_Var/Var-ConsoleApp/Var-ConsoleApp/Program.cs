78baa1659675361d207a77b9987fa3d5666cd1b6
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-04-27 14:32:56 +0200

    v1

---
+++ 02_Var/Var-ConsoleApp/Var-ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+string text;
+//een string is een reeks van tekens
+int number;
+//een int is een geheel getal positief of negatief
+double decimalnumber;
+//een double is een kommagetal
+bool boolean;
+//een bool is een waarde die true of false kan zijn
+
+var alletypes;
+//een var is een type dat automatisch wordt bepaald door de compiler
