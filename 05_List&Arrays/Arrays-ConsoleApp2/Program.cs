dd06b195e054a431deabc384b839ad6ac7c71974
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-04-28 23:22:22 +0200

    Printing 5 movies with arrays

--- 05_List&Arrays/Arrays-ConsoleApp2/Program.cs
+++ 05_List&Arrays/Arrays-ConsoleApp2/Program.cs
@@ -4,10 +4,13 @@ Console.WriteLine("Hello, World!");
 
 //Now lets create a program that will spit out a top 5 movie list
     string[] topMovies = { "The Godfather", "The Shawshank Redemption", "Schindler's List", "Raging Bull", "Casablanca" };
+//Stores 5 movies
     Console.WriteLine("Top 5 Movies of All Time");
     for (int i = 0; i < topMovies.Length; i++)
     {
         int rank= i + 1;
         Console.WriteLine(rank+". "+topMovies[i]);
     }
-//this will print out the top 5 movies of all time
+//loop that will print out the top 5 movies of all time
+
+Console.ReadKey();//Waits for key input before closing
