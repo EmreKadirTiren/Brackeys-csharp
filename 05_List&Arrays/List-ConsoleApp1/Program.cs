4bc35530c421314b5a7e81993ed0d60c61c4d872
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-04-28 23:36:37 +0200

    Grocery list app with a list

---
+++ 05_List&Arrays/List-ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to your shoppinglist please enter your 11 groceries");
+List<string> shoppinglist = new List<string>();//creates a list
+for (int i = 0; i < 11; i++)//for loop to insert groceries
+{
+    shoppinglist.Add(Console.ReadLine());//adds item to shoppinglist
+}
+
+for (int i = 0; i < 11; i++)//for loop to write groceries
+{
+    Console.WriteLine(shoppinglist[i]);
+}
+Console.ReadKey();//waits for input before closing
