2bd9697e9b949946bf3b879621865f142f07833e
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-04-30 15:46:17 +0200

    Wizard RPG Console Game I learned working with classes like Wizard.cs

---
+++ 07_Classes/Class-ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Class_ConsoleApp1;//imports the namespace Class_ConsoleApp1
+
+Console.WriteLine("Hello, World!\n");//prints "Hello, World!" to the console
+Wizard wizard01 = new Wizard("Dan", "Fireball", 3);//creates a new instance of the Wizard class
+//the constructor of the Wizard class is called with the arguments "Dan", "Fireball", 3
+
+for (int i = 0; i < 4; i++)
+{
+    wizard01.CastSpell();//calls the CastSpell method of the wizard01 object
+}
+
+wizard01.LongRest();
+//calls the LongRest method of the wizard01 object  
+
+for (int i = 0; i < 2; i++)
+{
+    wizard01.CastSpell();
+}
+
+Console.WriteLine(wizard01.name + " now has " + wizard01.experience + "experience");
+//prints the experience of the wizard01 object
+
+wizard01.levelUp();
+//calls the levelUp method of the wizard01 object
+
+Console.ReadKey();//pauses the program until a key is pressed
---
+++ 07_Classes/Class-ConsoleApp1/Wizard.cs
@@ -0,0 +1,56 @@
+﻿namespace Class_ConsoleApp1;
+
+public class Wizard
+{
+    public string name;
+    public string favoriteSpell;
+    public int originalSpellSlot;
+    public int spellSlot;
+    public float experience;
+    
+
+    public Wizard(string _name, string _favoriteSpell, int _spellSlot)
+    {
+        name = _name;
+        favoriteSpell = _favoriteSpell;
+        spellSlot = _spellSlot;
+        originalSpellSlot = _spellSlot;
+        experience = 0f;
+    }
+    
+public void CastSpell()
+    {
+        if (spellSlot > 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(name+" casts "+favoriteSpell);
+            spellSlot--;
+            experience += 0.3f;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{name} is out of spell slots, he must take a long rest");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        
+    }
+public void LongRest()
+    {
+        spellSlot = originalSpellSlot;
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(name + " has taken a long rest and has regained all "+spellSlot+" of his spell slots");
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+public void levelUp()
+    {
+        experience = 0f;
+        originalSpellSlot++;
+        spellSlot = originalSpellSlot;
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(name + " has leveled up and now has "+spellSlot+" spell slots");
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+}
