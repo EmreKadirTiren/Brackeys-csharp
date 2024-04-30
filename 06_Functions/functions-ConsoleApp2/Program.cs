9d9c1dd4e93ce08f7f5a5ce281bb3363193f6ebd
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-04-30 11:41:16 +0200

    Multiplier Program

---
+++ 06_Functions/functions-ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Meetalien();
+Meetalien();
+Meetalien();
+
+
+static void Meetalien()
+{
+    Random numberGen = new Random();
+
+    string name = "X-" + numberGen.Next(0, 1000);
+    int age = numberGen.Next(18, 1000);
+    Console.WriteLine("Hi I am" + name + "nice to meet you\ni am" + age + "yearls old");
+    Console.WriteLine("Oh and I am an alien");
+}
